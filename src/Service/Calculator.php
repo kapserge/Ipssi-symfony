ab0ce95f9f88653c7fcafaa867593933cfd245b0
kapserge <kapserge@gmail.com>
2019-06-25 09:55:19 +0200

    calcul

--- src/Service/Calculator.php
+++ src/Service/Calculator.php
@@ -9,7 +9,7 @@ class Calculator
     public function add(float $a, float $b): float
 
     {
-        return $a + $b + 2;
+        return $a + $b ;
     }
 
     public function divide(float $a, float $b): float
