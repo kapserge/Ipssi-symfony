dd1e562a794d311ea9971bc0afff387001a6922c
kapserge <kapserge@gmail.com>
2019-06-25 15:07:28 +0200

    calcul bien fini

--- src/Service/Calculator.php
+++ src/Service/Calculator.php
@@ -1,17 +1,13 @@
 <?php
+
 declare(strict_types=1);
+
 namespace App\Service;
 
-
 class Calculator
-
 {
     public function add(float $a, float $b): float
-
     {
         return $a + $b + 2;
     }
-
-    
-
 }
